1048c642934344d17a0baeec04ab69a45cd58a38
Ondřej Bárta <ondrej@ondrej.it>
2019-05-30 11:48:17 +0200

    Fix indents

--- modules/diary/views.py
+++ modules/diary/views.py
@@ -16,46 +16,46 @@ from radicale import Application, config
 
 
 class ApplicationResponse(HttpResponse):
-	def start_response(self, status, headers):
-		self.status_code = int(status.split(' ')[0])
-		for k, v in dict(headers).items():
-			self[k] = v
+    def start_response(self, status, headers):
+        self.status_code = int(status.split(' ')[0])
+        for k, v in dict(headers).items():
+            self[k] = v
 
 
 class RadicaleView(Application, View):
-	http_method_names = [
-		"delete",
-		"get",
-		"head",
-		"mkcalendar",
-		"mkcol",
-		"move",
-		"options",
-		"propfind",
-		"proppatch",
-		"put",
-		"report",
-	]
+    http_method_names = [
+        "delete",
+        "get",
+        "head",
+        "mkcalendar",
+        "mkcol",
+        "move",
+        "options",
+        "propfind",
+        "proppatch",
+        "put",
+        "report",
+    ]
 
-	def __init__(self, **kwargs):
+    def __init__(self, **kwargs):
 
-		configuration = config.load(extra_config=settings.RADICALE_CONFIG)
-		logger = logging.getLogger("diary")
+        configuration = config.load(extra_config=settings.RADICALE_CONFIG)
+        logger = logging.getLogger("diary")
 
-		super(RadicaleView, self).__init__(configuration, logger)
-		super(View, self).__init__(**kwargs)
+        super(RadicaleView, self).__init__(configuration, logger)
+        super(View, self).__init__(**kwargs)
 
-	def _read_raw_content(self, environ):
-		return environ["_body"]
+    def _read_raw_content(self, environ):
+        return environ["_body"]
 
-	@method_decorator(csrf_exempt)
-	def dispatch(self, request, *args, **kwargs):
-		environ = request.META
-		environ["_body"] = request.body
-		if not request.method.lower() in self.http_method_names:
-			return self.http_method_not_allowed(request, *args, **kwargs)
-		response = ApplicationResponse()
-		answer = self(environ, response.start_response)
-		for i in answer:
-			response.write(i)
-		return response
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        environ = request.META
+        environ["_body"] = request.body
+        if not request.method.lower() in self.http_method_names:
+            return self.http_method_not_allowed(request, *args, **kwargs)
+        response = ApplicationResponse()
+        answer = self(environ, response.start_response)
+        for i in answer:
+            response.write(i)
+        return response
